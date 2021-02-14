3dd1c818c3ae57284cd9ffb36965d189a3c5574f
Enes İğneci <eigneci@magiclick.com>
2021-02-14 21:25:51 +0300

    AddBasket button added the data to local storage on IO thread

--- app/src/main/java/com/enesigneci/basket/ui/listing/ListingAdapter.kt
+++ app/src/main/java/com/enesigneci/basket/ui/listing/ListingAdapter.kt
@@ -7,8 +7,12 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.enesigneci.basket.R
 import com.enesigneci.basket.databinding.ListingItemLayoutBinding
+import com.enesigneci.basket.db.BasketDb
 import com.enesigneci.basket.extensions.loadFromUrl
 import com.enesigneci.basket.model.Listing
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListingAdapter(private var fragment: Fragment) : RecyclerView.Adapter<ListingAdapter.ListingViewHolder>(){
     var items: ArrayList<Listing.ListingItem> = arrayListOf()
@@ -27,6 +31,9 @@ class ListingAdapter(private var fragment: Fragment) : RecyclerView.Adapter<List
                 binding.tvCartPrice.text = price.plus(" ₺")
                 binding.ivCartImage.loadFromUrl(image)
                 binding.btnAddCart.setOnClickListener {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        BasketDb.db.basketDao().insert(items[position])
+                    }
                     fragment.findNavController().navigate(R.id.action_global_cartFragment)
                 }
             }
